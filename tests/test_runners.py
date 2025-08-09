de42d67cec10f2a21d0763f9c8c28938e771b780
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-08-09 11:37:54 +0100

    fix udocker runner and test

--- tests/test_runners.py
+++ tests/test_runners.py
@@ -1,20 +1,33 @@
+import importlib
 from pathlib import Path
+
+import pytest
 
 from jules_tools.runners import JulesExeRunner, JulesUdockerRunner
 
-
-def test_exe():
-    jules_exe = (
+jules_exe = Path(
         "/home/joe/github.com/NERC-CEH/portable-jules/_build/build/bin/jules.exe"
     )
+
+@pytest.mark.skipif(not jules_exe.exists(), reason="Only run on developer's computers for now.")
+def test_exe():
     jules = JulesExeRunner(jules_exe)
 
     jules(exec_dir=Path(__file__).parent / "data" / "experiment", namelists_subdir="namelists")
 
+def udocker_installed() -> bool:
+    try:
+        _ = importlib.import_module("udocker")
+        return True
+    except ModuleNotFoundError:
+        return False
 
-def _test_udocker():
+@pytest.mark.skipif(not udocker_installed(), reason="udocker is not installed.")
+def test_udocker():
+    # TODO: run udocker.UdockerCLI.do_ps() and check that jules is an existing 
+    # container before running this test?
     container_name = "jules"
 
     jules = JulesUdockerRunner(container_name)
 
-    jules(namelists_dir="loobos/config", run_dir="loobos")
+    jules(exec_dir=Path(__file__).parent / "data" / "experiment", namelists_subdir="namelists")
